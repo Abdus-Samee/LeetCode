d2e945064fba9a9093d5403f0cd0e1c43f2be1b6
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2020-09-21 12:05:27 +0600

    Removed redundancy

--- String/Generate a String With Characters That Have Odd Counts.cpp
+++ String/Generate a String With Characters That Have Odd Counts.cpp
@@ -2,16 +2,12 @@ class Solution {
 public:
     string generateTheString(int n) {
         string s = "";
-        if(n%2){
-            for(int i = 1; i <= n; i++){
-                s += 'a';
-            }
-        }else{
-            for(int i = 1; i < n; i++){
-                s += 'a';
-            }
-            s += 'b';
+        
+        for(int i = 1; i < n; i++){
+            s += 'a';
         }
+        
+        if(n%2) s += 'b';
         
         return s;
     }
